c25f042e155fc30234925cad15032cd4a8ddc27a
ruiamcosta <ram666.rc@gmail.com>
2025-04-07 23:21:54 +0100

    add app layouts

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,19 @@ import { Route, Routes } from 'react-router-dom'
 
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { AppLayout } from './pages/layouts/app'
+import { AuthLayout } from './pages/layouts/auth'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/" element={<Dashboard />} />
+      </Route>
+
+      <Route path="/" element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignIn />} />
+      </Route>
     </Routes>
   )
 }
